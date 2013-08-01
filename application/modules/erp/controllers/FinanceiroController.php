b6ea107eca80bb6bcc14aa92d95cda89a39687f4
Daniel Chaves <daniel@danielchaves.com.br>
2013-08-01 18:32:14 -0300

    Alteradas referencias do modulo financeiro

--- application/modules/erp/controllers/FinanceiroController.php
+++ application/modules/erp/controllers/FinanceiroController.php
@@ -1,10 +1,41 @@
 <?php
+/**
+ * Este arquivo é parte do projeto SysAdmin - ERP em PHP
+ *
+ * Este programa é um software livre: você pode redistribuir e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU (GPL)como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior
+ * e/ou sob os termos da Licença Pública Geral Menor GNU (LGPL) como é publicada pela
+ * Fundação para o Software Livre, na versão 3 da licença, ou qualquer versão posterior.
+ *
+ * Este programa é distribuído na esperança que será útil, mas SEM NENHUMA
+ * GARANTIA; nem mesmo a garantia explícita definida por qualquer VALOR COMERCIAL
+ * ou de ADEQUAÇÃO PARA UM PROPÓSITO EM PARTICULAR,
+ * veja a Licença Pública Geral GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Publica GNU e da
+ * Licença Pública Geral Menor GNU (LGPL) junto com este programa.
+ * Caso contrário consulte <http://www.fsfla.org/svnwiki/trad/GPLv3> ou
+ * <http://www.fsfla.org/svnwiki/trad/LGPLv3>.
+ *
+ * @package    	Cepheros
+ * @subpackage 	SysAdmin
+ * @name		FinanceiroContoller
+ * @version   	1.0.75
+ * @license   	http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @copyright 	2009-2012 &copy; SysAdmin
+ * @link      	http://www.cepheros.com.br
+ * @author    	Daniel R. Chaves <dchaves at 32dll dot com dot br>
+ *
+ */
 class Erp_FinanceiroController extends Zend_Controller_Action{
 
 	public $log;
 	public $configs;
 	public $cache;
 	public $userInfo;
+	
+	
 	
 	public function init(){
 	
