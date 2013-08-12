dc707056fafe6ae8751d54ddf1d3466bb01a34e5
Daniel Chaves <daniel@danielchaves.com.br>
2013-08-12 18:41:13 -0300

    Alteracoes no composer

--- application/modules/dashboard/controllers/IndexController.php
+++ application/modules/dashboard/controllers/IndexController.php
@@ -103,6 +103,33 @@ class Dashboard_IndexController extends Zend_Controller_Action{
 		
 	}
 	
+	
+	
+	public function testsAction(){
+		
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender();
+	
+	
+	$sacado = new  OpenBoleto\Agente('Fernando Maia', '023.434.234-34', 'ABC 302 Bloco N', '72000-000', 'Brasília', 'DF');
+	$cedente = new OpenBoleto\Agente('Empresa de cosméticos LTDA', '02.123.123/0001-11', 'CLS 403 Lj 23', '71000-000', 'Brasília', 'DF');
+
+	$boleto = new OpenBoleto\Banco\BancoDoBrasil(array(
+    // 	Parâmetros obrigatórios
+    	'dataVencimento' => new DateTime('2013-01-24'),
+    	'valor' => 23.00,
+    	'sequencial' => 1234567, // Para gerar o nosso número
+    	'sacado' => $sacado,
+    	'cedente' => $cedente,
+    	'agencia' => 1724, // Até 4 dígitos
+    	'carteira' => 18,
+    	'conta' => 10403005, // Até 8 dígitos
+    	'convenio' => 1234, // 4, 6 ou 7 dígitos
+	));
+	
+	echo $boleto->getOutput();
+	
+	}
 
 	
 }
